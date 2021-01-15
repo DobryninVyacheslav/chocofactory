aa774e3b957a45d4af4514c57c06d2b9309fba0c
Vyacheslav Dobrynin <dobrinin_v_j@ntik.ru>
2021-01-15 22:22:17 +0300

    refactoring

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,4 @@
-INSERT INTO request(ingredients, quantity, date) values ('Орехи', 1000, CURRENT_TIMESTAMP);
 INSERT INTO usr(username,password,active) values ('admin','p', true);
-INSERT INTO user_role(user_id, roles) values (1, 'ADMIN')
+INSERT INTO user_role(user_id, roles) values (1, 'ADMIN');
+INSERT INTO usr(username,password,active) values ('user','p', true);
+INSERT INTO user_role(user_id, roles) values (2, 'USER');
