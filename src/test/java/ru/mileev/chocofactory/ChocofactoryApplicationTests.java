e0dfb2b7ba15b6d46eb7f6f5555ece52cd832523
Vyacheslav Dobrynin <dobrinin_v_j@ntik.ru>
2021-01-16 09:57:55 +0300

    delete test annotations

--- src/test/java/ru/mileev/chocofactory/ChocofactoryApplicationTests.java
+++ src/test/java/ru/mileev/chocofactory/ChocofactoryApplicationTests.java
@@ -1,12 +1,9 @@
 package ru.mileev.chocofactory;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
+//@SpringBootTest
 class ChocofactoryApplicationTests {
 
-    @Test
+//    @Test
     void contextLoads() {
     }
 
